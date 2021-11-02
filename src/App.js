e9c99bc933cf2c1fa6441ea2f8772450efcfaffb
nahuelfacundo <nahh.com@gmail.com>
2021-11-02 17:52:07 -0300

    navBar está ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import "./components/NavBar.js";
+import NavBar from "./components/NavBar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
 		<>
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-				</header>
-			</div>
+			<NavBar />
 		</>
 	);
 }
